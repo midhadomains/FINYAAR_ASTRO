94ca744d9ae7cd301b4ad455f02684d4674c69e6
Midha <developer@midha.in>
2026-09-04 13:55:28 +0530

    sitmap update

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development
 const { SITE_URL } = loadEnv(mode, process.cwd(), '');
 
 export default defineConfig({
-  site: SITE_URL || 'https://finyaar.com',
+  site: SITE_URL || 'https://www.finyaar.com',
   integrations: [sitemap({ filter: (page) => !page.includes('/internal/') })],
   vite: {
     plugins: [tailwindcss()]
